8b008e0a1310ff1d396a0da473d18487e1548ccb
Cr02y <waynesun1990@gmail.com>
2017-04-13 00:40:34 +0800

    split listview into view and cell.

--- ListViewDemo/js/RNDetail.js
+++ ListViewDemo/js/RNDetail.js
@@ -9,9 +9,15 @@ import {
 export default class RNDetail extends Component {
 	constructor(props) {
 		super(props);
+		// {navigator} = this.props.navigator;
 	}
 
 	render() {
-		return (<Text>this is a detail page</Text>)
+		const {navigator} = this.props
+		return (
+			<View style = {{paddingTop: 64}}>
+				<Text style = {{height: 44}}>this is a detail page</Text>
+			</View>
+		);
 	}
 }
--- ListViewDemo/js/cell/RNTableViewCell.js
+++ ListViewDemo/js/cell/RNTableViewCell.js
@@ -2,9 +2,56 @@ import React, {Component} from 'react'
 import {
 	Image,
 	Text,
-	View
+	View,
+	StyleSheet,
+	TouchableHighlight
 } from 'react-native'
 
 export default class RNTableViewCell extends Component {
-	
+
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+	}
+
+
+	render() {
+		return (
+			<TouchableHighlight onPress = {() => {
+				
+			}}
+				<View style = {styles.cellcontainer}>
+
+				</View>
+			</TouchableHighlight>
+		);
+	}
 }
+
+
+var styles = StyleSheet.create({
+	image: {
+		marginLeft: 8,
+		height: 26,
+		width:26,
+		flex: 1,
+	}
+
+	title: {
+		marginTop: 8,
+		fontSize: 18,
+		color: '#333333',
+	}
+
+	description: {
+		marginBottom: 2,
+		fontSize: 12,
+		color: '#666666',
+	}
+
+	cellcontainer: {
+		
+	}
+});
